e35fe614b8586b78c65b042f0fe5a5e696028632
Vlad Getman <ibrightsider@gmail.com>
2016-11-16 15:19:36 +0200

    rework login manager and add types

--- Source/LoginManager.swift
+++ Source/LoginManager.swift
@@ -18,6 +18,9 @@ public typealias LoginCompletion = (_ response: LoginResponse) -> Void
     case facebook
     case twitter
     case soundCloud
+    case instagram
+    case linkedIn
+    case tumblr
 }
 
 public func StringFromAuthType(_ authType: AuthType) -> String {
@@ -36,16 +39,22 @@ public func StringFromAuthType(_ authType: AuthType) -> String {
         
     case .soundCloud:
         return "SoundCloud"
+        
+    case .instagram:
+        return "Instagram"
+        
+    case .linkedIn:
+        return "LinkedIn"
+        
+    case .tumblr:
+        return "Tumblr"
     }
 }
 
 @objc public protocol LoginProtocol: NSObjectProtocol {
     func request(_ completion: @escaping LoginCompletion)
     func authType() -> AuthType
-    func hasAuthData() -> Bool
-    func authData() -> [String : Any]
     func authDataFromResponse(_ response: Any) -> [String : Any]
-    func save(_ response: Any)
     func reset()
 }
 
@@ -54,7 +63,7 @@ public func StringFromAuthType(_ authType: AuthType) -> String {
     func userDidLoggedOut()
 }
 
-open class AuthCredentials: NSObject {
+open class AuthCredentials: NSObject, NSSecureCoding {
     
     open var type: AuthType
     open var data: [String : Any]
@@ -63,6 +72,23 @@ open class AuthCredentials: NSObject {
         self.type = authType
         self.data = authData
         super.init()
+    }
+    
+    // MARK: NSCoding
+    
+    public required init?(coder aDecoder: NSCoder) {
+        self.type = AuthType(rawValue: aDecoder.decodeInteger(forKey: "type"))!
+        self.data = aDecoder.decodeObject(forKey: "data") as! [String : Any]
+        super.init()
+    }
+    
+    public static var supportsSecureCoding: Bool {
+        return true
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(type.rawValue, forKey: "type")
+        aCoder.encode(data, forKey: "data")
     }
 }
 
@@ -88,6 +114,8 @@ open class LoginManager: NSObject {
         }
     }
     
+    fileprivate static let credentialsKey = "HILoginManager.Credentials"
+    
     fileprivate class func extensionForType(_ authType: AuthType) -> LoginProtocol {
         let loginExtension = LoginManager.extensions.filter({ $0.authType() == authType })
         assert(loginExtension.count > 0, "You doesn't register a login extension for \(StringFromAuthType(authType))")
@@ -98,25 +126,27 @@ open class LoginManager: NSObject {
         extensionForType(authType).request(completion)
     }
     
-    open class func hasAuthData() -> Bool {
-        let loginExtension = LoginManager.extensions.filter({ $0.hasAuthData() == true })
-        return loginExtension.count > 0
+    open class func hasCredentials() -> Bool {
+        return KeychainSwift().getData(credentialsKey) != nil
     }
     
-    open class func authData() -> AuthCredentials {
-        let loginExtension = LoginManager.extensions.filter({ $0.hasAuthData() == true }).first
-        return AuthCredentials(authType: loginExtension!.authType(), authData: loginExtension!.authData())
+    open class func credentials() -> AuthCredentials {
+        let data = KeychainSwift().getData(credentialsKey)!
+        return NSKeyedUnarchiver.unarchiveObject(with: data) as! AuthCredentials
     }
     
-    open class func saveAuthData(_ response: Any, authType: AuthType) {
-        extensionForType(authType).save(response)
+    open class func saveCredentials(_ credentials: AuthCredentials) {
+        let data = NSKeyedArchiver.archivedData(withRootObject: credentials)
+        KeychainSwift().set(data, forKey: credentialsKey)
     }
     
     open class func logOut() {
-        for (_, loginExtension) in LoginManager.extensions.enumerated() {
-            loginExtension.reset()
+        
+        for item in extensions {
+            item.reset()
         }
-        KeychainSwift().clear()
+        
+        KeychainSwift().delete(credentialsKey)
         
         if let cookies = HTTPCookieStorage.shared.cookies {
             for cookie in cookies {
